2dfea183b6b1741ad94b7663bb500c74d2fc0a1f
balajistyle1962 <77094465+balajistyle1962@users.noreply.github.com>
2021-06-13 11:38:59 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,44 @@
+print('python bootcamp')
+print("30 days 30 hour challenge")
+print('30 days 30 hours challenge')
+#assigning string to variable
+Hours="thirty"
+print(Hours)
+
+
+Days="Thirty days"
+print(Days[0])
+print(Days[5])
+
+Challenge="I will win"
+#length of String
+print(len(Challenge))
+#print particular character from certain text
+print(Challenge[1:5])
+print(Challenge[7:10])
+#String into lower character
+print(Challenge.lower())
+#String concatenation
+a="30 Days"
+b="30 hours"
+c=a+" "+b
+print(c)
+
+#casefold usage
+text="I am Balaji.N.R."
+x=text.casefold()
+print(x)
+
+#Capitalize() usage
+print(x.capitalize())
+
+#find() usage
+print(x.find("Hi"))
+print(x.find("Balaji",5,12))
+
+#check whether alphabets
+print(x.isalpha())
+
+#check whether alpha numeric
+print(x.isalnum())
+
