e7387b5804bf5cea3742fbc6d02a9e69a754b227
balajistyle1962 <77094465+balajistyle1962@users.noreply.github.com>
2021-06-15 13:17:31 +0530

    Day 4

---
+++ Day4.py
@@ -0,0 +1,32 @@
+print("Best Enlist Bootcamp Python 2021\n Balaji.N.R.")
+#Assigning to a same value
+print("Enter value:")
+a=b=c=int(input())
+print(a,b,c)
+a=a/10
+print(a)
+b*=50
+print(b)
+c+=60
+print(c)
+print("**************************************\n")
+
+#Create a String variable of 5 characters and replace the 3rd character with G
+print("Enter a string of 5 characters:")
+str=input()
+arr= [char for char in str]
+arr[2]='G'
+s=''.join(arr)
+print(s)
+print("**************************************\n")
+
+#type conversion
+print("Enter value of a (in int):")
+a=int(input())
+print("Enter value of b (in float):")
+b=float(input())
+print(a,b)
+print("Type conversion:")
+print(float(a))
+print(int(b))
+print("**************************************\n")
