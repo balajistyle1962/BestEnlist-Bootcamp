072aa623c3ee8bbf35aa026626f7f9a7fcfabdb1
balajistyle1962 <77094465+balajistyle1962@users.noreply.github.com>
2021-06-23 12:42:45 +0530

    Day 11

---
+++ Day11.py
@@ -0,0 +1,25 @@
+print("BestEnlist Bootcamp Python 2021\nBalaji.N.R.")
+from list import listfinal
+n=int(input("Enter number to be added to all elements in the list:"))
+print(listfinal(n))
+print("*"*25)
+
+
+import pandas as pd
+cars = {"Names": ["AUDI","BENZ","BMW"],"Price": [12000,36000,45000]}
+var = pd.DataFrame(cars)
+print(var)
+print("*"*25)
+
+import random
+n = random.randint(1,100)
+print(n)
+print("*"*25)
+
+import sys
+print(sys.executable)
+print("*"*25)
+
+
+
+
